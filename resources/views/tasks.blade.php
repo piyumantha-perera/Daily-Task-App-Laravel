ec6d43ddd47f8f6670d3ae35e464b7c93dc92a78
piyumantha perera <41514689+piyumantha-perera@users.noreply.github.com>
2020-06-05 22:22:36 +0530

    table created

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="text-center">
+            <h1>Daily Tasks</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" name="task" placeholder="Enter the task here:">
+                    <br>
+                    <input type="button" class="btn btn-warning" value="SAVE">
+                    <input type="button" class="btn btn-primary" value="CLEAR">
+                    <br>
+                    <br>
+                    <br>
+                    
+                    <table class="table table-dark">
+                    <th>ID</th>
+                    <th>TASK</th>
+                    <th>COMPLETED</th>
+                    <tr></tr>
+                    <td>1</td>
+                    <td>Laravel</td>
+                    <td>not yet</td>
+                    </tr>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
